02d17ab30f38ddb3a0940e057f091e055e85a22e
samueldob <frankdobisamuel@gmail.com>
2025-04-16 10:01:36 +0100

    installed react router dom and set up the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 // import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import Vote from './Components/Vote';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   
 
   return (
+    <Router>
     <div className="w-full">
       <Navbar/>
+      <div className='content-body'>
+        <Routes>
+          <Route path='/' element= {<Home/>}/>
+          <Route path='/vote' element= {<Vote/>}/>
+        </Routes>
+      </div>
     </div>
+    </Router>
   )
 }
 
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -8,11 +8,10 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: 'Home', href: '#', current: true },
+  { name: 'Vote', href: '#', current: false },
+  { name: 'Login ', href: '#', current: false },
+  { name: 'Register', href: '#', current: false },
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
