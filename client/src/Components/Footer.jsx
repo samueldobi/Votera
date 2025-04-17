fcc78361356ab7f435c43f29da8472e2d1c0d1c6
samueldob <frankdobisamuel@gmail.com>
2025-04-17 09:47:20 +0100

    made changes to the footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,68 +1,155 @@
 import React from 'react'
 
 const Footer = () => {
+  const socialLinks = [
+    { 
+      name: 'Facebook',
+      icon: (
+        <svg fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+          />
+        </svg>
+      ),
+    },
+    { 
+      name: 'Instagram',
+      icon: (
+        <svg fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+          />
+        </svg>
+      ),
+    },
+    { 
+      name: 'X',
+      icon: (
+        <svg fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6">
+          <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5549 21H20.7996L13.6823 10.6218ZM11.5541 13.0782L10.8883 12.0661L5.31989 4.28194H7.70053L12.1422 10.5225L12.8079 11.5346L18.6861 19.7204H16.3055L11.5541 13.0782Z" />
+        </svg>
+      ),
+    },
+    { 
+      name: 'GitHub',
+      icon: (
+        <svg fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          />
+        </svg>
+      ),
+    },
+    { 
+      name: 'YouTube',
+      icon: (
+        <svg fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z"
+          />
+        </svg>
+      ),
+    },
+  ];
+
+  const solutions = ['Marketing', 'Analytics', 'Automation', 'Commerce', 'Insights'];
+  const support = ['Submit ticket', 'Documentation', 'Guides'];
+  const company = ['About', 'Blog', 'Jobs', 'Press'];
+  const legal = ['Terms of service', 'Privacy policy', 'License'];
+
   return (
-    <>
-        <footer className="footer sm:footer-horizontal bg-gray-700 text-neutral-content p-10">
-  <aside>
-    <svg
-      width="50"
-      height="50"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      fillRule="evenodd"
-      clipRule="evenodd"
-      className="fill-current">
-      <path
-        d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-    </svg>
-    <p>
-      Built by innova
-      <br />
-      Providing reliable tech 24/7
-    </p>
-  </aside>
-  <nav>
-    <h6 className="footer-title">Social</h6>
-    <div className="grid grid-flow-col gap-4">
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-        </svg>
-      </a>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-        </svg>
-      </a>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-        </svg>
-      </a>
-    </div>
-  </nav>
-</footer>
-    </>
-  )
+    <footer className="footer bg-gray-900 text-white">
+      <div className="mx-auto max-w-7xl px-6 py-12  md:flex md:items-center md:justify-between lg:px-8">
+        <div className=" md:max-w-md lg:max-w-lg">
+          <div className="">
+            <img
+              alt="Company name"
+              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&amp;shade=500"
+              className="h-8 w-auto"
+            />
+            <p className="mt-2 text-sm text-gray-300">
+              Vote. Decide. Empower
+            </p>
+            <div className="mt-8 flex space-x-6">
+              {socialLinks.map((social) => (
+                <a key={social.name} href="#" className="text-gray-400 hover:text-gray-300">
+                  <span className="sr-only">{social.name}</span>
+                  {social.icon}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+               {/* Footer links */}
+        <div className="">
+             {/* Footer Links */}
+             <div className="mt-12 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold text-white">Solutions</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {solutions.map((item) => (
+                    <li key={item}>
+                      <a href="#" className="text-sm text-gray-300 hover:text-white">{item}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold text-white">Support</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {support.map((item) => (
+                    <li key={item}>
+                      <a href="#" className="text-sm text-gray-300 hover:text-white">{item}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold text-white">Company</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {company.map((item) => (
+                    <li key={item}>
+                      <a href="#" className="text-sm text-gray-300 hover:text-white">{item}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold text-white">Legal</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {legal.map((item) => (
+                    <li key={item}>
+                      <a href="#" className="text-sm text-gray-300 hover:text-white">{item}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Footer links */}
+    
+        
+      </div>
+
+      {/*  */}
+          <div className="mt-12 ml-5 mr-5 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between text-center">
+          <p className="text-sm text-gray-400 r">© 2025 Innova Digital, Inc. All rights reserved.</p>
+        </div>
+      {/*  */}
+    </footer>
+  );
 }
 
 export default Footer
