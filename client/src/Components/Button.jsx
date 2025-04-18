6f448f8d7cec13aee503a3e177a44e348f1ca3f6
samueldob <frankdobisamuel@gmail.com>
2025-04-18 15:23:43 +0100

    added multistep indicators

--- client/src/Components/Button.jsx
+++ client/src/Components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({text, href, onClick}) => {
         <a
             href={href}
             onClick={onClick}
-            className="vote-now-btn rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="vote-now-btn rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
             >
             {text}
           
