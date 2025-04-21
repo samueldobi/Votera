53a3db219500135d4353dea2db39f90f648059bb
samueldob <frankdobisamuel@gmail.com>
2025-04-21 22:47:50 +0100

    added a delete button to delete participants

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require ('express');
 const app = express();
 
+// Connect to mongodb
+const dbURI = "mongodb+srv://Votera-chief:HGNHmeOCS6Cmmpri@votera.sso1xui.mongodb.net/?retryWrites=true&w=majority&appName=Votera";
+
 // set up api
 app.get("/api", (req,res)=>{
     res.json({ users: ["john", "twin"]})
