66d5df899ccfcfe9ac5d2a7c1916b63a94c97c53
samueldob <frankdobisamuel@gmail.com>
2025-04-29 16:08:04 +0100

    restructured server files in an MVC pattern

--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -125,7 +125,7 @@ const handleSubmit = async(e) =>{
           <div>
             <button
               type="submit"
-              className="orange-color-bg flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="orange-color-bg flex w-full justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 cursor-pointer "
             >
               Register
             </button>
