aa67439c9ac749804b0e6f04efa05c7790d6c1cb
samueldob <frankdobisamuel@gmail.com>
2025-05-28 11:45:45 +0100

    added new styling to home page

--- client/src/Components/CallToAction.jsx
+++ client/src/Components/CallToAction.jsx
@@ -3,16 +3,16 @@ export default function CallToAction() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex justify-center lg:gap-x-20 lg:px-24 lg:pt-0">
           <CtaIcon/>
-          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto py-32 px-2">
             <h2 className="text-3xl font-semibold tracking-tight text-balance text-white sm:text-4xl">
               Boost your productivity. Start using our app today.
             </h2>
             <p className="mt-6 text-lg/8 text-pretty text-gray-300">
               Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+            <div className="mt-10 flex items-center justify-center gap-x-6 ">
               <a
                 href="#"
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-xs hover:bg-gray-100 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
