06e94571b5c23891523da565f723e95b5594849d
samueldob <frankdobisamuel@gmail.com>
2025-06-02 16:41:01 +0100

    traced handlelogout issue

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Navigate, useNavigate } from 'react-router-dom'
+import axios from "axios"
 
 const user = {
   name: 'Tom Cook',
@@ -17,14 +19,29 @@ const navigation = [
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
+  { name: 'Sign out', href: null },
 ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
+// API URL FOR BACKEND CALLS
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const handleLogout = async()=>{
+    try{
+      const response = await axios.post(`${apiUrl}/logout`,null, {withCredentials:true,})
+      if(response.status === 200){
+        navigate('/');
+      }
+      console.log(response)
+    }catch(err){
+      console.log(err)
+    }
+    console.log('button has been clicked')
+  }
   return (
     <>
       <div className="w-full navbar">
@@ -45,7 +62,8 @@ export default function Navbar() {
                 </div>
                 <div className="hidden md:block">
                   <div className="ml-10 flex items-baseline space-x-4">
-                    {navigation.map((item) => (
+                    {navigation.map((item) => 
+                   (
                       <a
                         key={item.name}
                         href={item.href}
@@ -57,6 +75,7 @@ export default function Navbar() {
                       >
                         {item.name}
                       </a>
+                      
                     ))}
                   </div>
                 </div>
@@ -85,7 +104,9 @@ export default function Navbar() {
                       transition
                       className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
                     >
-                      {userNavigation.map((item) => (
+                      {userNavigation.map((item) => 
+                 
+                      (
                         <MenuItem key={item.name}>
                           <a
                             href={item.href}
@@ -147,7 +168,18 @@ export default function Navbar() {
                 </button>
               </div>
               <div className="mt-3 space-y-1 px-2">
-                {userNavigation.map((item) => (
+                {userNavigation.map((item) => 
+                item.name === 'Sign out'?
+                    <DisclosureButton
+                    key={item.name}
+                    onClick={handleLogout}
+                    as="a"
+                    // href={item.href}
+                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                  >
+                    {item.name}
+                  </DisclosureButton>
+                :(
                   <DisclosureButton
                     key={item.name}
                     as="a"
