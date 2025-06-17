e68ef7efa5db9d5f7f5dcefe1d0011cb71cbf38b
samueldob <frankdobisamuel@gmail.com>
2025-06-17 23:15:05 +0100

    fixing commit issues

--- server/server.js
+++ server/server.js
@@ -58,14 +58,14 @@ mongoose.connect(dbURI)
   .then(() => app.listen(5000, ()=>{console.log("mongodb connected succesfully")}))
   .catch((err) => console.error("MongoDB connection error:", err));
   
-<<<<<<< HEAD:server/index.js
 
-=======
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
->>>>>>> demo-branch:server/server.js
+
 // Routes for user registration
 app.use(authRoutes);
 // Routes for poll functions
